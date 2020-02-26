b30e5eb30ef68ece846cac237000b8ca219fc9ce
Magnus Conrad Hyll <magnus@hyll.no>
2020-02-26 12:12:46 +0100

    Testing Dockerized ap

---
+++ ElliotLib/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ElliotLib
+{
+	public class DbContext
+	{
+		public string GetData()
+		{
+			return "Dummy data";
+		}
+
+		public void SaveData(string data)
+		{
+			// Noop
+		}
+	}
+}
